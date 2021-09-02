6f4455dff6ed3c9f3d986898515c2f9049d9141e
rdperry031 <rdperry031@gmail.com>
2021-09-02 17:38:30 -0500

    forgot to push

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,15 +5,24 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+    render(<ContactForm/>)
 });
 
 test('renders the contact form header', ()=> {
+    render(<ContactForm/>)
+    const header = screen.queryByText(/contact form/i)
     
+    expect(header).toBeInTheDocument();
+    expect(header).toHaveTextContent(/contact form/i)
+    expect(header).toBeTruthy()
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+    render(<ContactForm/>)
+    const firstNameInput = screen.getByLabelText(/first name*/i)
+    userEvent.type(firstNameInput, 'Liz')
+    const firstNameFeedback = await screen.findByText(/Error:/);
+    expect(firstNameFeedback).toBeInTheDocument();
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
